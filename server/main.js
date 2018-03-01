f5ea7acebb0d79575655da2de128bb5173d2e7c0
Harikrishna Mekala <haribabbu.krishna@gmail.com>
2018-03-01 11:03:11 +0530

    Json Header Has been added

--- server/main.js
+++ server/main.js
@@ -1,18 +1,25 @@
 var fs = require('fs');
 var express = require('express');
+var bodyParser = require('body-parser')
 
 var app = express();
+    app.use(bodyParser.json())
+    app.use(bodyParser.urlencoded({
+        extended: true }
+    ))
 
-    app.post('/', function(req, res){
+
+    app.post('/filename', function(req, res){
         console.log(req.body)
         res.send({
-            Msg : "File Name Received Awating File Data"
+            Msg : "File Name Received Awating File Data",
+            Data : req.body
         })
     })
 
     app.post('/filepost', function (req, res, next) {
         req.pipe(fs.createWriteStream('./uploadFile'));
         req.on('end', next);
-    });
+    })
 
-    app.listen(3000);
+    app.listen(3000)
